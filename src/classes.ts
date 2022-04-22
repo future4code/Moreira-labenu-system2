8d072d69655d17ee7a0fd3b475f1655a31e28a3c
Eliel Mariano <eliel.mariano@engenharia.ufjf.br>
2022-04-22 13:54:16 -0300

    criação das tabelas

---
+++ src/classes.ts
@@ -0,0 +1,30 @@
+
+
+export class Turma {
+
+}
+
+
+class Usuário {
+
+}
+
+
+interface Hobbies {
+
+}
+
+
+export class Estudante extends Usuário implements Hobbies {
+
+}
+
+
+interface Especialidades {
+
+}
+
+
+export class Docente extends Usuário implements Especialidades {
+
+}
